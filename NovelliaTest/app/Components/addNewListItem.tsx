e307b7b51b7d4fe6c4f187b2a18831d8d37cdb34
Anna Karwacki <avkarwacki@gmail.com>
2025-07-07 17:46:57 -0400

    date picker added

--- NovelliaTest/app/Components/addNewListItem.tsx
+++ NovelliaTest/app/Components/addNewListItem.tsx
@@ -4,6 +4,7 @@ import AppButton from "./appButton";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { setIsAddPetOpen } from "@/Redux/reducers/SystemSettings";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function AddNewListItem({itemType, onClose}: any){
     const dispatch = useDispatch();
@@ -13,17 +14,25 @@ export default function AddNewListItem({itemType, onClose}: any){
     const [type, setType] = useState('');
     const [breed, setBreed] = useState('');
     const [DOB, setDOB] = useState('');
+    const [date, setDate] = useState(new Date(1598051730000))
+    const [openDate, setOpenDate] = useState(false)
 
     const isModalOpen = useSelector((store: any)=> {
         return store.systemSettings.isAddPetOpen;
     });
     const [modalVisible, setModalVisible] = useState(isModalOpen);
-    useEffect(() => {
-    setModalVisible(isModalOpen);
-    console.log("isModalOpen: " + isModalOpen);
-}, [isModalOpen]);
+        useEffect(() => {
+        setModalVisible(isModalOpen);
+        console.log("isModalOpen: " + isModalOpen);
+    }, [isModalOpen]);
 
-    
+    const onChange = (event:any, selectedDate:Date) => {
+    const currentDate = selectedDate;
+    //setShow(false);
+    setDate(currentDate);
+    setDOB(currentDate.getMonth().toString() + "-" + currentDate.getDate().toString() + "-" + currentDate.getFullYear().toString());
+    console.log(DOB)
+  };
     return(
         <Modal 
           animationType="slide"
@@ -63,13 +72,19 @@ export default function AddNewListItem({itemType, onClose}: any){
                         placeholder="Your Pet's Breed"
                         placeholderTextColor={"#807e7c"}
                     />
-                    <TextInput
-                        style={styles.shortTextInput}
-                        onChangeText={setDOB}
-                        value={DOB}
-                        placeholder="Your Pet's DOB"
-                        placeholderTextColor={"#807e7c"}
-                    />
+                    <View style={{margin: 'auto'}}>
+                        <Text style={styles.bodyText}>Select Your Pet's Birthday</Text>
+                        <View style={{ alignContent: 'center', margin: 20, left: 30, width: '100%', paddingRight:20}}>
+                            <DateTimePicker
+                                style={{width: '100%', margin:'auto', }}
+                                testID="dateTimePicker"
+                                value={date}
+                                mode={'date'}
+                                onChange={onChange}
+                                themeVariant={"light"}
+                            />
+                        </View>
+                    </View>
                     <View style={{margin:'auto'}}>
                         <AppButton style={styles.loginButton} text={"Save"} onPress={()=>{dispatch(setIsAddPetOpen({isAddPetOpen: false}))}}/>
                         <AppButton style={styles.loginButton} text={"Cancel"} onPress={()=>{dispatch(setIsAddPetOpen({isAddPetOpen: false}))}}/>
